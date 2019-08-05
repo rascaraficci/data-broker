0fac6cc4719147a4aaaf7eee66ac62ca8642ab49
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2019-08-05 12:04:43 -0300

    Isolate tests from external dependencies

--- tests/TopicBuilder.test.ts
+++ tests/TopicBuilder.test.ts
@@ -4,24 +4,35 @@
 import "jest";
 import { TopicManagerBuilder } from "../src/TopicBuilder";
 
+jest.mock("../src/topicManager", () => ({
+  TopicManager: jest.fn((service: string) => {
+    // part of default TopicManager execution flow
+    if ((service === undefined) || service.length === 0) {
+      throw new Error();
+    }
+  }),
+}));
+
 describe("TopicBuilder", () => {
   const topicBuilder = TopicManagerBuilder;
 
-  it("should build a new service - valid name", () => {
-    expect(topicBuilder.get("testService1")).toBeDefined();
-  });
+  describe("get", () => {
+    it("should build a new service - valid name", () => {
+      expect(topicBuilder.get("testService1")).toBeDefined();
+    });
 
-  it("should retrieve an already created service", () => {
-    const manager = topicBuilder.get("testService2");
-    expect(manager).toBeDefined();
+    it("should not build a new service - invalid name", () => {
+      expect(() => topicBuilder.get("")).toThrow();
+    });
 
-    const retrievedManager = topicBuilder.get("testService2");
-    expect(retrievedManager).toBeDefined();
+    it("should retrieve an already created service", () => {
+      const manager = topicBuilder.get("testService2");
+      expect(manager).toBeDefined();
 
-    expect(manager).toBe(retrievedManager);
-  });
+      const retrievedManager = topicBuilder.get("testService2");
+      expect(retrievedManager).toBeDefined();
 
-  it("should not build a new service - invalid name", () => {
-    expect(() => topicBuilder.get("")).toThrow();
+      expect(manager).toBe(retrievedManager);
+    });
   });
 });
